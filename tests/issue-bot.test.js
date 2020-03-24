3e86fae030363e1ca9d7c2c620b36eaa612085cf
John Bohannon <imjohnbo@github.com>
2020-03-24 10:35:15 -0400

    testing: initial commit

--- tests/issue-bot.test.js
+++ tests/issue-bot.test.js
@@ -1,3 +1,20 @@
-describe('Weekly Radar', () => {
-	it('exits successfully', () => {});
+const core = require('@actions/core');
+const { GitHub, context } = require('@actions/github');
+const run = require('../src/issue-bot');
+
+jest.mock('@actions/core');
+jest.mock('@actions/github');
+jest.mock('js-yaml');
+
+describe('Issue Bot', () => {
+
+	let issueBot;
+
+	beforeEach(() => {
+
+	});
+
+	it('runs', () => {
+		run();
+	});
 });
